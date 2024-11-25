d26382b4c0cac1d74cbfb28dca9499ea9869103a
Christian Baumann <christian.baumann@bd.so.ch>
2024-11-25 16:15:34 +0100

    search functions

---
+++ queries/trgm_search_ebene_function.sql
@@ -0,0 +1,73 @@
+-- SELECT * 
+-- FROM agi_suchindex_v1.trgm_search_ebene(
+--      ARRAY['Grund', 'wasser']
+-- );
+
+CREATE OR REPLACE FUNCTION agi_suchindex_v1.trgm_search_ebene(
+    search_terms TEXT[],       		-- Array von Suchbegriffen
+    limit_results INT DEFAULT 200  	-- Maximalanzahl der Ergebnisse
+)
+RETURNS TABLE (
+    anzeige TEXT,
+    identifier TEXT,
+    hat_beschreibung BOOLEAN,
+    unter_ebenen TEXT,
+    sml NUMERIC
+) AS $$
+DECLARE
+    dynamic_query TEXT;				-- Zum Speichern der dynamischen SQL-Abfrage
+      term_conditions TEXT; 			-- Dynamische WHERE-Bedingungen für Suchbegriffe
+       similarity_calculation TEXT;	-- Berechnung der Similarity-Summe
+BEGIN
+    -- Dynamische WHERE-Bedingungen für alle Suchbegriffe erstellen
+    -- Bedingungen für LIKE:
+    SELECT STRING_AGG(
+        format(
+            '(
+            suchbegriffe_p1 LIKE ''%%%s%%'' OR 
+            suchbegriffe_p2 LIKE ''%%%s%%'' OR 
+            suchbegriffe_p3 LIKE ''%%%s%%''
+            )',
+            lower(term), lower(term), lower(term)
+        ),
+        ' AND '
+    ) 
+    INTO term_conditions
+    FROM unnest(search_terms) AS term;
+       
+    -- Dynamische Berechnung der Similarity-Summe erstellen
+    SELECT STRING_AGG(
+        format(
+            'COALESCE(similarity(suchbegriffe_p1, ''%s''), 0) * 100 + ' ||
+            'COALESCE(similarity(suchbegriffe_p2, ''%s''), 0) * 10 + ' ||
+            'COALESCE(similarity(suchbegriffe_p3, ''%s''), 0)',
+            lower(term), lower(term), lower(term)
+        ),
+        ' + '
+    ) INTO similarity_calculation
+    FROM unnest(search_terms) AS term;
+    
+       -- Dynamische SQL-Abfrage erstellen
+    dynamic_query := format(
+        'SELECT 
+            anzeige::TEXT,
+            identifier::TEXT,
+            hat_beschreibung,
+            unter_ebenen::TEXT,
+            CAST(%s AS NUMERIC) AS sml
+        FROM 
+            agi_suchindex_v1.ebene
+        WHERE 
+            %s
+        ORDER BY 
+            sml DESC
+        LIMIT 
+            %s',
+        similarity_calculation,  	-- Dynamische Similarity-Berechnung
+        term_conditions,        	-- Dynamische Suchbedingungen
+        limit_results				-- Begrenzung der Ergebnisse
+    );
+
+    -- Query ausführen
+    RETURN QUERY EXECUTE dynamic_query;
+END $$ LANGUAGE plpgsql;
--- queries/trgm_search_feature_function.sql
+++ queries/trgm_search_feature_function.sql
@@ -1,6 +1,18 @@
+-- SELECT * 
+-- FROM agi_suchindex_v1.trgm_search_feature(
+--     ARRAY['Zuch', 'Kirch'], 
+--     ARRAY['ch.so.agi.av.gebaeudeadressen.gebaeudeeingaenge']
+-- );
+-- 
+-- SELECT * 
+-- FROM agi_suchindex_v1.trgm_search_feature(
+--     ARRAY['Zuch','Kirch'], 
+--     NULL
+-- );
+
 CREATE OR REPLACE FUNCTION agi_suchindex_v1.trgm_search_feature(
     search_terms TEXT[],          -- Array von Suchbegriffen
-    layer_filters TEXT[],         -- Array von Layernamen
+    layer_filters TEXT[],         -- Array von Layernamen, NULL für alle Layer
     limit_results INT DEFAULT 200 -- Maximalanzahl der Ergebnisse
 )
 RETURNS TABLE (
@@ -21,11 +33,11 @@ BEGIN
     -- Dynamische WHERE-Bedingungen für Suchbegriffe erstellen
     SELECT STRING_AGG(
         format(
-        	'(
-			suchbegriffe LIKE ''%%%s%%'' 
-			AND 
-			similarity(''%s'', suchbegriffe) > 0
-			)', lower(term), lower(term)),
+            '(
+            suchbegriffe LIKE ''%%%s%%'' 
+            AND 
+            similarity(''%s'', suchbegriffe) > 0
+            )', lower(term), lower(term)),
         ' AND '
     )
     INTO term_conditions
@@ -40,30 +52,40 @@ BEGIN
     FROM unnest(search_terms) AS term;
 
     -- Dynamische WHERE-Bedingungen für Layernamen erstellen
-    SELECT STRING_AGG(format('''%s''', layer), ', ')
-    INTO layer_condition
-    FROM unnest(layer_filters) AS layer;
-
+    IF array_length(layer_filters, 1) IS NOT NULL THEN
+        -- Wenn Layer-Filter angegeben sind
+        SELECT 
+            STRING_AGG(quote_literal(layer), ', ')
+        INTO 
+            layer_condition
+        FROM 
+               unnest(layer_filters) AS layer;
+        layer_condition := format('layer_ident IN (%s)', layer_condition);
+    ELSE
+        -- Wenn keine Layer-Filter (also NULL) angegeben sind, alle Layer einbeziehen
+        layer_condition := 'TRUE'; -- Bedingung ist immer wahr
+    END IF;
+   
     -- Dynamische SQL-Abfrage erstellen
     dynamic_query := format(
-    	'SELECT 
-        	anzeige::text AS anzeige, 
+        'SELECT 
+            anzeige::text AS anzeige, 
             layer_ident::text AS layer_ident, 
-			id_spalten_name::text AS id_spalten_name,
-			id_feature::text AS id_feature,
-			ausdehnung::text AS ausdehnung,
-			%s AS srid,
+            id_spalten_name::text AS id_spalten_name,
+            id_feature::text AS id_feature,
+            ausdehnung::text AS ausdehnung,
+            %s AS srid,
             %s AS sml
         FROM 
-			agi_suchindex_v1.feature
+            agi_suchindex_v1.feature
         WHERE 
-			layer_ident IN (%s) 
-           	AND 
-			%s
-     	ORDER BY 
-			sml DESC
-    	LIMIT 
-			%s',
+            %s 
+               AND 
+            %s
+         ORDER BY 
+            sml DESC
+        LIMIT 
+            %s',
         quote_literal('EPSG:2056'),
         similarity_calculation, -- Dynamische Similarity-Summe
         layer_condition,        -- Dynamische Layer-Bedingungen

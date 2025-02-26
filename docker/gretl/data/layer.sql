1a0c769d745e0444f548465e679e260c1dc8e269
Christian Baumann <christian.baumann@bd.so.ch>
2025-02-26 15:17:52 +0100

    adapted layer sql to new model

--- docker/gretl/data/layer.sql
+++ docker/gretl/data/layer.sql
@@ -2,7 +2,7 @@ SELECT
     trim('" ' FROM (string_to_array(trim('[]' FROM id), ','))[2]) AS identifier,
     display AS anzeige,
     REPLACE(REPLACE(dset_children, '"ident":','"dataproduct_id":'),'"subclass":','"type":') AS unter_ebenen, --TODO: CHANGE IN solr VIEW, NOT here
-    (facet = 'background') AS ist_hintergrund,
+    facet AS layertyp,
     dset_info as hat_beschreibung,
     lower(search_1_stem) as suchbegriffe_p1,
     lower(search_2_stem) as suchbegriffe_p2,
